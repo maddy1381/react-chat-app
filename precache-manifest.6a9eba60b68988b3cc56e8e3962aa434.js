474ae841555e76aebc0023cefdf6a64a421b1605
Madhav Sharma <83121276+maddy1381@users.noreply.github.com>
2021-10-08 11:17:03 +0530

    Add files via upload

---
+++ precache-manifest.6a9eba60b68988b3cc56e8e3962aa434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03c4a178eafb8a294d1e0cabb802675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad58482a101a7e9359e6",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "9f16669a78388b1c7e2d",
+    "url": "/static/js/2.479629ae.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.479629ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad58482a101a7e9359e6",
+    "url": "/static/js/main.7592e0ea.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
